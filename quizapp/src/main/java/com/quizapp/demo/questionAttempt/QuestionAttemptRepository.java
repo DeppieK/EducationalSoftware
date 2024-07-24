72d6423bd264ea11addc2365229edd7e6fe0254a
DeppieK <deppie4@gmail.com>
2024-07-24 10:35:59 +0300

    corrected code

--- quizapp/src/main/java/com/quizapp/demo/questionAttempt/QuestionAttemptRepository.java
+++ quizapp/src/main/java/com/quizapp/demo/questionAttempt/QuestionAttemptRepository.java
@@ -1,8 +1,14 @@
 package com.quizapp.demo.questionAttempt;
 
+import com.quizapp.demo.question.Question;
+import com.quizapp.demo.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionAttemptRepository extends JpaRepository<QuestionAttempt, Long> {
+    int countByQuestionAndUser(Question question, User user);
+    Optional<QuestionAttempt> findByUserAndQuestion(User user, Question question);
 }
